3dceadce98f5d84daab9addabc5afc584de77f5a
tomaspecl <tomaspecl@email.cz>
2021-06-27 11:45:14 +0200

    Copyright license should be correct now

--- src/clip_box.rs
+++ src/clip_box.rs
@@ -1,4 +1,5 @@
 // Copyright 2020 The Druid Authors.
+// Modified by Tomáš Pecl.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,20 @@
+/*
+Visual asm editor
+Copyright (C) 2021  Tomáš Pecl
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 const JUMPS: &[&str] = &["jmp"];
 const CONDITIONAL_JUMPS: &[&str] = &["ja", "jae", "jb", "jb", "jbe", "jc", "jcxz", "jecxz", "jrcxz", "je", "jg", "jge", "jl", "jle", "jna", "jnae", "jnb", "jnbe", "jnc", "jne", "jng", "jnge", "jnl", "jnle", "jno", "jnp", "jns", "jnz", "jo", "jp", "jpe", "jpo", "js", "jz"];
 
